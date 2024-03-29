86f2105a63bb4596ad493deb0752a493bb1f0c5d
Kharavho Ndou <kharavhondou@kharavhondou-macair.local>
2024-03-29 17:39:56 +0200

    PLP Dart Technical Assignment

---
+++ android/app/src/main/kotlin/com/example/dart_technical_assignment/MainActivity.kt
@@ -0,0 +1,5 @@
+package com.example.dart_technical_assignment
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity()
---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  // Demonstration of int data type
+  int age = 26;
+
+  // Demonstration of double data type
+  double temperature = 25.5;
+
+  // Demonstration of String data type
+  String name = "Kharavho Ndou";
+
+  // Demonstration of List data type
+  List<int> numbers = [1, 2, 3, 4, 5];
+
+  // Demonstration of Map data type
+  Map<String, String> capitalCities = {
+    "South Africa": "Johannesburg",
+    "Kenya": "Nairobi",
+    "Tanzania": "Dodoma",
+  };
+
+  // Printing out the values
+  print("Age: $age");
+  print("Temperature: $temperature");
+  print("Name: $name");
+  print("Numbers: $numbers");
+  print("Capital Cities: $capitalCities");
+}
